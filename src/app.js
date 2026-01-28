1a8758b465d33510bdbff93aae32f501bdedfb2a
Asif Iqbal <aass0iiff03@gmail.com>
2026-01-28 22:40:20 +0530

    fix: add default export to app.js

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
@@ -37,3 +37,4 @@ app.use("/api/v1/dashboard", dashboardRouter)
 app.use("/api/v1/healthcheck", healthcheckRouter)
 
 export { app }
+export default app
