1066d1376ed6eb3c0b8f4a1f12d2165de1fb4f5a
Asif Iqbal <aass0iiff03@gmail.com>
2026-01-28 22:32:45 +0530

    chup

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import connectDB from "../src/db/index.js";
+import { app } from "../src/app.js";
+
+dotenv.config({ path: './.env' });
+
+connectDB()
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed", err);
+    });
+
+export default app;
